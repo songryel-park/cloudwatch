6969bd5c59882f82ebe1a1a642edd5acbef055d3
Park Sung-Ryel <105186195+songryel-park@users.noreply.github.com>
2024-02-29 14:16:34 +0900

    Update ErrorTest.kt
    error log

--- src/main/kotlin/org/example/slacklogs/ErrorTest.kt
+++ src/main/kotlin/org/example/slacklogs/ErrorTest.kt
@@ -8,6 +8,13 @@ import org.springframework.web.bind.annotation.RestController
 class ErrorTest {
     private val logger = LoggerFactory.getLogger(ErrorTest::class.java)
 
+    @Scheduled(fixedRate = 60000) // 1분마다 실행
+    fun logErrors() {
+        repeat(10) {
+            logger.error("에러")
+        }
+    }
+
     @GetMapping("/test")
     fun doSomething() {
         logger.error("에러")
